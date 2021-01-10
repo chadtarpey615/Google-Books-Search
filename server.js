94a959f6e139e92f56e3267ad2e968b9072c461a
Chad Tarpey <chadtarpey@gmail.com>
2021-01-09 20:10:59 -0600

    added mongoose connect

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.listen(PORT, function() {
     console.log(` API Server now listening on port ${PORT}!`);
